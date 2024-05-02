62b7a9752ea56e090dd89a136ea46c65f61847d0
Takibalu <takibalu18@gmail.com>
2024-05-02 08:33:10 +0200

    GitHub usage - Working arrow

---
+++ Assets/ArrowPlacer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowPlacer : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+    public GameObject arrowPrefab;
+
+    private GameObject currentArrow;
+    public float displayDuration = 2f; // A nyíl megjelenésének ideje másodpercekben
+    public float delayBetweenDisplays = 1f; // A megjelenések közötti késleltetés másodpercekben
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlaceArrow();
+    }
+
+    void PlaceArrow()
+    {
+        if (startPoint != null && endPoint != null && arrowPrefab != null)
+        {
+            Vector3 direction = endPoint.position - startPoint.position;
+            Vector3 arrowPosition = startPoint.position + direction / 2f;
+
+            Quaternion rotationOffset = Quaternion.Euler(0, 90, 0); 
+            Quaternion rotation = Quaternion.LookRotation(direction) * rotationOffset;
+
+            float distance = direction.magnitude;
+            Vector3 scale = new Vector3(20f *distance, 60f, 60f);
+
+            arrowPrefab.transform.localScale = scale;
+
+            currentArrow = Instantiate(arrowPrefab, arrowPosition, rotation);
+
+
+            Destroy(currentArrow, displayDuration);
+
+            Invoke("PlaceArrow", displayDuration + delayBetweenDisplays);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/PrintSimulation.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintSimulation : MonoBehaviour
+{
+    public ArrowPlacer arrowPlacer;
+    public float downloadDuration = 2f;
+    public float waitDuration = 2f;
+    public float printDuration = 2f;
+    public Transform PCPoint;
+    public Transform PrinterPoint;
+    public Transform RouterPoint;
+    public GameObject Paper;
+
+    void Start()
+    {
+        InvokeRepeating("PrintProcess", 0f, downloadDuration + printDuration+waitDuration);
+    }
+
+    void PrintProcess()
+    {
+        StartCoroutine(DownloadAndPrint());
+    }
+
+    IEnumerator DownloadAndPrint()
+    {
+
+        Paper.SetActive(false);
+
+        arrowPlacer.startPoint = RouterPoint;
+        arrowPlacer.endPoint = PCPoint;
+        arrowPlacer.gameObject.SetActive(true);
+        yield return new WaitForSeconds(downloadDuration);
+
+        arrowPlacer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(waitDuration); 
+
+        arrowPlacer.startPoint = PCPoint;
+        arrowPlacer.endPoint = PrinterPoint;
+        arrowPlacer.gameObject.SetActive(true);
+        yield return new WaitForSeconds(printDuration);
+
+        arrowPlacer.gameObject.SetActive(false);
+
+
+        yield return new WaitForSeconds(waitDuration);
+        Paper.SetActive(true);
+        yield return new WaitForSeconds(waitDuration);
+        Paper.SetActive(false);
+     
+    }
+}
+
